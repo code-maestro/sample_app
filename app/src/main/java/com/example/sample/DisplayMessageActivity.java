5ff91e21a6af3f4975381a58b4cdf793ecf3f51b
ATWIINE GILBERT <michaelajnew@gmail.com>
2020-02-06 16:47:22 +0300

    Hello world

---
+++ app/src/main/java/com/example/sample/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("MESSAGE");
+        TextView messageView = findViewById(R.id.messageTextView);
+
+        messageView.setText(message);
+    }
+    public void onClose(View view) {
+
+        super.finish();
+
+    }
+
+    }
--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        EditText message = findViewById(R.id.message);
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        intent.putExtra("MESSAGE", message.getText().toString());
+
+        startActivity(intent);
+
+        message.setText("");
     }
 }
