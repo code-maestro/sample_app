7f93617b955a37e9f538ad606d18996daab6d74e
ATWIINE GILBERT <michaelajnew@gmail.com>
2020-03-11 09:30:05 +0300

    implemented the alarm..

--- app/src/main/java/com/example/sample/MyReceiver.java
+++ app/src/main/java/com/example/sample/MyReceiver.java
@@ -3,6 +3,8 @@ package com.example.sample;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
 
@@ -10,6 +12,14 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+
+        // TOAST DISPALYING THE END OF THE ALARM
+        Toast toast = Toast.makeText(context, "Time is UP dwagg", Toast.LENGTH_LONG);
+
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
