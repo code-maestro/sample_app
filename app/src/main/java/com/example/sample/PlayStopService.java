5ee07f6ca3f6ee17dbec212c9946b3af29541fe8
code_maestro <michaelajnew@gmail.com>
2020-12-07 15:00:56 +0300

    Added the foreground service notification functionality

--- app/src/main/java/com/example/sample/PlayStopService.java
+++ app/src/main/java/com/example/sample/PlayStopService.java
@@ -1,18 +1,28 @@
 package com.example.sample;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 
-// AN UNBOUND SERVICE(Started service) for playing music in the back ground
+import androidx.core.app.NotificationCompat;
 
+import static com.example.sample.Notification.CHANEL_ID;
+
+// AN UNBOUND SERVICE(Started service) for playing music in the back ground
 public class PlayStopService extends Service {
 
     private MediaPlayer mPlayer;
+    private Notification mNotification;
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
+    @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
@@ -20,19 +30,38 @@ public class PlayStopService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
 
-        mPlayer = MediaPlayer.create(this, R.raw.higher);
-        mPlayer.start();
+        playSong();
 
-        return START_STICKY;
+        notificationStuff();
+
+        return START_NOT_STICKY;
 
     }
 
+    private void playSong(){
+        mPlayer = MediaPlayer.create(this, R.raw.higher);
+        mPlayer.start();
+    }
+
+    private void notificationStuff(){
+        Intent songUIintent = new Intent(this,Song.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, songUIintent, 0);
+
+        mNotification = new NotificationCompat.Builder(this, CHANEL_ID)
+                .setContentTitle("ForegroundService Example")
+                .setContentText("music playing in the back ground")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, mNotification);
+    }
+    
     @Override
     public void onDestroy(){
         super.onDestroy();
-
         mPlayer.stop();
-
     }
 }
 
--- app/src/main/java/com/example/sample/Song.java
+++ app/src/main/java/com/example/sample/Song.java
@@ -1,5 +1,7 @@
 package com.example.sample;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
